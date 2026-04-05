3e278f4a6b6682e90cf9f3e3706ea689076314fa
samithspalan <samithpoojary12345@gmail.com>
2026-04-05 12:23:14 +0530

    test 16

--- test2.js
+++ test2.js
@@ -7,12 +7,4 @@ function findLargest(numbers) {
   for (let i = 1; i < numbers.length; i++) {
     if (numbers[i] > largest) {
       largest = numbers[i]
-    }
-  }
-  console.log("Numbers:", numbers);
-  console.log("Largest number:", largest)
-}
-
-let scores = [80, 70, 90, 60, 85];
-console.log("Scores:", scores);
-findLargest(scores);
+ 
