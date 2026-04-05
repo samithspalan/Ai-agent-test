69455af12599113131eb3f3c7e1424663bfcb226
samithspalan <samithpoojary12345@gmail.com>
2026-04-05 11:32:14 +0530

    test 16

--- test2.js
+++ test2.js
@@ -1,19 +1,11 @@
 function findLargest(numbers) {
-  let largest = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
+  let largest = numbers[0]
+
+  for (let i = 1; i <= numbers.length; i++) {
     if (numbers[i] > largest) {
-      largest = numbers[i];
+      largest = number[i]
     }
   }
-  console.log("Numbers:", numbers);
-  console.log("Largest number:", largest);
-  if (largest % 2 === 0) {
-    console.log("The largest number is even");
-  } else {
-    console.log("The largest number is odd");
-  }
-  return largest;
-}
 
-let values = [12, 45, 7, 89, 34, 22];
-findLargest(values);
+  console.log("Numbers:", number);
+  console.log("Largest number:", largest)
